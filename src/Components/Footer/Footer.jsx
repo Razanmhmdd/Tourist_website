52447db8243d8d58a8257bad1d361b6af55fcef7
razan <razanrazanrish@gmail.com>
2024-10-29 10:37:42 +0530

    img path change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <footer className="footer bg-base-200 text-base-content p-10" style={{ backgroundImage: `url(${bgImg})` }}>
   <nav>
     <img src={logo} alt="Logo" className="h-12 w-auto" />
-    <a className="link link-hover">No. 15/1/C,</a>
+    <a className="link link-hover ">No. 15/1/C,</a>
     <a className="link link-hover">New Digana Road, Kundasale,</a>
     <a className="link link-hover">Kandy, SriLanka</a>
     <a className="link link-hover">Tel :- +94772250223</a>
